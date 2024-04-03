c217bcee045608467198884f51315326ac245bf2
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2024-04-03 22:58:59 +0200

    perf(bench): run without shell

--- bench.sh
+++ bench.sh
@@ -3,10 +3,10 @@ set -eu -o pipefail
 
 wget -O '/tmp/in.webm' 'https://cdn.media.ccc.de/congress/2017/webm-sd/34c3-8710-deu-eng-Relativitaetstheorie_fuer_blutige_Anfaenger_webm-sd.webm'
 
-hyperfine 'ffmpeg -y -v error -i /tmp/in.webm -c copy -codec:v libx264 -t 0:15 -f mp4 /dev/null'
-hyperfine 'ffmpeg -y -v error -i /tmp/in.webm -c copy -codec:v libx265 -t 0:15 -f mp4 /dev/null'
+hyperfine --shell=none 'ffmpeg -y -v error -i /tmp/in.webm -c copy -codec:v libx264 -t 0:15 -f mp4 /dev/null'
+hyperfine --shell=none 'ffmpeg -y -v error -i /tmp/in.webm -c copy -codec:v libx265 -t 0:15 -f mp4 /dev/null'
 
 cd meeting-countdown
 
-hyperfine --warmup 1 'touch src/main.rs && cargo build'
-hyperfine 'cargo clean && cargo build --release'
+hyperfine --shell=none --warmup 1 --prepare 'touch src/main.rs' 'cargo build'
+hyperfine --shell=none --prepare 'cargo clean' 'cargo build --release'
