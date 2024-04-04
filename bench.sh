678312719fedfd7452110d6a69854e8683f466b0
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2024-04-04 18:36:58 +0200

    feat: use plain base image
    this simplifies adapting to usage on a host system directly

--- bench.sh
+++ bench.sh
@@ -6,7 +6,7 @@ wget -O '/tmp/in.webm' 'https://cdn.media.ccc.de/congress/2017/webm-sd/34c3-8710
 hyperfine --shell=none 'ffmpeg -y -v error -i /tmp/in.webm -c copy -codec:v libx264 -t 0:15 -f mp4 /dev/null'
 hyperfine --shell=none 'ffmpeg -y -v error -i /tmp/in.webm -c copy -codec:v libx265 -t 0:15 -f mp4 /dev/null'
 
-cd meeting-countdown
+cd build-rust
 
 hyperfine --shell=none --warmup 1 --prepare 'touch src/main.rs' 'cargo build'
 hyperfine --shell=none --prepare 'cargo clean' 'cargo build --release'
--- clone.sh
+++ clone.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 
-git clone https://github.com/EdJoPaTo/meeting-countdown
-cd meeting-countdown
+git clone https://github.com/EdJoPaTo/meeting-countdown build-rust
+cd build-rust
 git checkout 32e6619e8080e21447b3bf4952a5862149b510ec
 rm -rf .git .github
+
+rustup override set 1.77
